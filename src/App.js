1367bed7d092a30348a02e209a6602b3efebc9f1
Lakshay Munjal <lakshay.munjal22@gmail.com>
2022-05-10 04:01:11 +0530

    created button to start the quiz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
+function clickme(){
+  alert('you clicked');
+}
+
 function App() {
   return (
     <div className="App">
-      hikkkkk
+      <button onClick={clickme}>
+        
+        button
+
+      </button>
+            
     </div>
   );
 }
